ff1b3f3f57b62b07c2b9450f618f03cb2ce8fbf7
Electronic Panda <electronicpanda@gmail.com>
2025-03-31 01:00:14 +0300

    1- removed keys hehe 2- Added music video 3- gitignore change manually, android studio doesn't have "add to gitignore" option it seems.
    powered by ChatGPT(lyrics) and SunoAI(music/vocals)

--- app/src/main/java/com/onder/youareawesome/MainActivity.kt
+++ app/src/main/java/com/onder/youareawesome/MainActivity.kt
@@ -1,47 +1,55 @@
 package com.onder.youareawesome
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+import com.onder.youareawesome.R
 import com.onder.youareawesome.ui.theme.YouAreAwesomeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             YouAreAwesomeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                VideoPlayer(videoResId = R.raw.youareawesome) // Pass the raw resource ID
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "You Are Awesome!!!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YouAreAwesomeTheme {
-        Greeting("Android")
+fun VideoPlayer(videoResId: Int) {
+    val context = androidx.compose.ui.platform.LocalContext.current
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context).build().apply {
+            val uri = Uri.parse("android.resource://${context.packageName}/$videoResId")
+            setMediaItem(MediaItem.fromUri(uri))
+            prepare()
+            playWhenReady = true // Auto-play
+        }
     }
+
+    AndroidView(
+        factory = {
+            PlayerView(it).apply {
+                player = exoPlayer
+                useController = false // Hide controls for fullscreen effect
+                layoutParams = FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
 }
